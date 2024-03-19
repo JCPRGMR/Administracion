a869a5142b2f5afbf257681cd92f3fd50b3596c0
juancardedon@gmail.com <juancardedon@gmail.com>
2024-03-19 17:33:13 -0400

    1. Terminar las interfaces del sistemas [PENDIENTE]

---
+++ view/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/style1.css">
+    <link rel="stylesheet" href="../css/menus.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="color m0 f-row w100p h100vh">
+    <input type="checkbox" name="" id="nav_on" class="v-hidden absolute chk_menu">
+    <nav class="transition02 h100p color7 white f-col a-c">
+        <div class="pic100 p20">
+            <img src="../rtp_img/rtp_logo_blanco.png" alt="imagen XD">
+        </div>
+        <ul class="p0 m0 h100p">
+            <a href="#" class="white">
+                <li class="mayus p10 hover-white center fz12">
+                    Omisiones, permisos y marcaciones
+                </li>
+            </a>
+            <a href="" class="white">
+                <li class="mayus p10 hover-white center fz12">
+                    Ingresos y salida de personal
+                </li>
+            </a>
+        </ul>
+    </nav>
+    <main class="color2 w100p">
+        <div class="cabecera color6 p10 mayus negrita f-row a-c noselect"> 
+            <div class="burguer f-row gap10 f-row a-c">
+                <label class="pointer" for="nav_on">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+                    </svg>
+                </label>
+                Administracion 
+            </div>
+        </div>
+        <div class="cuerpo">
+            tablas
+        </div>
+    </main>
+</body>
+</html>
